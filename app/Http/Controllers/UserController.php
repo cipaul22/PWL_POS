c7523bf3239dc2f56d883df16e2686830757fd50
cipaul22 <cipaismunir10@gmail.com>
2025-05-03 00:49:22 +0700

    JS4-Prak2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,24 +1,32 @@
 <?php
+
 namespace App\Http\Controllers;
- 
- use App\Models\m_user;
- use Illuminate\Http\Request;
- use Illuminate\Support\Facades\Hash;
- 
- class UserController extends Controller
- {
-     public function index()
-     {   
+
+use App\Models\m_user;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
         $user = m_user::firstOrNew(
             [
-                'username'=>'manager33',
-                'nama'=>'Manager Tiga Tiga',
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
-            ],
+            ]
         );
+        $user->username = 'manager303';
+
         $user->save();
-        return view('user',['data'=>$user]);
-     }
-     
- }
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged('nama');
+        $user->wasChanged(['nama', 'username']);
+        dd($user->wasChanged(['nama', 'username']));
+    }
+}
